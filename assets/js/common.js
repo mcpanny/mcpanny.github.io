a1292921f690fa075eb6f746debaf676be993151
Panhia Vang (Pan) <68967090+mcpanny@users.noreply.github.com>
2021-10-27 16:21:14 -0700

    redesign portfolio using bootstrap

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,53 @@
 $(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
+	window.onscroll = function() {checkScroll()};
+	var divScrollBtn = document.getElementById("divScrollBtn");
+	var navbar = document.getElementById("navbar");
+	
+	function checkScroll() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+		{			
+			divScrollBtn.style.visibility = "visible";	
+			divScrollBtn.style.opacity = 1;
+			navbar.classList.remove("bg-light");
+			navbar.classList.add("bg-white");
+			navbar.classList.add("custom-navbar");
+			
+		}
+		else
+		{
+			divScrollBtn.style.visibility = "hidden";	
+			divScrollBtn.style.opacity = 0;
+			navbar.classList.add("bg-light");
+			navbar.classList.remove("bg-white");
+			navbar.classList.remove("custom-navbar");
+		}
+	}
+	
+	checkScroll();
+	function setButtonText(btn, message) {
+		$(btn).tooltip('hide')
+		.attr('data-original-title', message)
+		.tooltip('show');
+	}
+
+	function resetButtonText(btn) {
+		setTimeout(function() {
+			$(btn).tooltip('hide');
+		}, 1000);
+	}
+	
+	var clipboard = new ClipboardJS("#btnEmail");
+	clipboard.on('success', function(e) {	
+		$('#btnEmail').text('copied!');	
+		setTimeout(function() {
+			$('#btnEmail').text('copy');
+		}, 1000);
+	});
+
+	clipboard.on('error', function(e) {		
+		$('#btnEmail').text('failed :(');
+		setTimeout(function() {
+			$('#btnEmail').text('copy');
+		}, 1000);
+	});
 });
